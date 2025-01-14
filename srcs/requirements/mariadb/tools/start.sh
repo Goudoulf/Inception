22f6671b7b6d65774ac59d2b388cb29b0998485d
cassie <cassie@cassie.42.fr>
2025-01-14 03:38:33 -0600

    final

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -23,7 +23,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         DELETE FROM mysql.db WHERE Db='test';
         DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASSWORD}';
+	GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MDB_ROOT_PASSWORD'; FLUSH PRIVILEGES;
 
         CREATE DATABASE ${MDB_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
         CREATE USER '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_USER_PASSWORD}';
--- srcs/requirements/wordpress/tools/wp-launch.sh
+++ srcs/requirements/wordpress/tools/wp-launch.sh
@@ -6,7 +6,8 @@ IFS=$'\n\t'
 : "${MDB_HOSTNAME:?Need to set MDB_HOSTNAME}"
 : "${MDB_USER:?Need to set MDB_USER}"
 : "${MDB_USER_PASSWORD:?Need to set MDB_USER_PASSWORD}"
-: "${MDB_ROOT_PASSWORD:?Need to set MDB_ROOT_PASSWORD}"
+: "${WP_ROOT:?Need to set WP_ROOT}"
+: "${WP_ROOT_PASSWORD:?Need to set WP_ROOT_PASSWORD}"
 : "${MDB_DATABASE:?Need to set MDB_DATABASE}"
 : "${DOMAIN_NAME:?Need to set DOMAIN_NAME}"
 
@@ -18,7 +19,7 @@ then
 else
 wp-cli.phar core download --allow-root
 wp-cli.phar config create --dbname=$MDB_DATABASE --dbuser=$MDB_USER --dbpass=$MDB_USER_PASSWORD --dbhost=$MDB_HOSTNAME --allow-root
-wp-cli.phar core install --url=$DOMAIN_NAME --title=Inception --admin_user=$MDB_ROOT --admin_password=$MDB_ROOT_PASSWORD --admin_email=cassie@student.42lyon.fr --allow-root
+wp-cli.phar core install --url=$DOMAIN_NAME --title=Inception --admin_user=$WP_ROOT --admin_password=$WP_ROOT_PASSWORD --admin_email=cassie@student.42lyon.fr --allow-root
 wp-cli.phar theme activate twentytwentyfour --allow-root
 
 fi
